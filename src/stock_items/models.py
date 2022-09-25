26356197e639144f9d780e0fb331bffa98e7f663
Andrey Almosov <aa@addcode.ru>
2022-09-25 22:17:30 +0300

    Update stock_items.models.py: - Added integer validators - Minor code style changes

--- src/stock_items/models.py
+++ src/stock_items/models.py
@@ -1,7 +1,9 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 from app.models import DefaultModel
 
 
+# TODO: add logging for status change (status changed to ...)
 class StockItem(DefaultModel):
     class STATUS(models.TextChoices):
         NEW = 'Новое'
@@ -15,24 +17,22 @@ class StockItem(DefaultModel):
     name = models.CharField('Наименование', max_length=128)
     manufacturer = models.ForeignKey('stock_items.Manufacturer', verbose_name='Производитель', on_delete=models.PROTECT)
     model = models.ForeignKey('stock_items.ItemModel', verbose_name='Модель', on_delete=models.PROTECT)
-    amount = models.PositiveIntegerField('Количество')
+    amount = models.PositiveIntegerField('Количество', validators=[MinValueValidator(1)])
     state = models.CharField('Статус', max_length=64, choices=STATUS.choices, default=STATUS.NEW)
-
-    def save(self, *args, **kwargs) -> None:
-        # TODO: add logs
-        super().save(args, kwargs)
 
     class Meta:
         verbose_name = 'Вещь на складе'
         verbose_name_plural = 'Вещи на складе'
 
 
+# TODO: add logging for StockItem (used x<amount> of <repair_item> #<repair_item.id> for <repairing_item>)
+# TODO: add delete signal
 class Repairs(DefaultModel):
-    repairing_item = models.ForeignKey('stock_items.StockItem', related_name='repairs', on_delete=models.CASCADE)  # TODO: add delete signal
+    repairing_item = models.ForeignKey('stock_items.StockItem', related_name='repairs', on_delete=models.CASCADE)
     repair_item = models.ForeignKey('stock_items.StockItem', related_name='in_use_repairs', verbose_name='Запчасть', on_delete=models.PROTECT)
-    amount = models.PositiveIntegerField('Количество использованных запчастей')
+    amount = models.PositiveIntegerField('Количество использованных запчастей', validators=[MinValueValidator(1)])
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'#{self.repair_item.id} {self.repair_item.name}' \
                f' -> ' \
                f'#{self.repairing_item.id} {self.repairing_item.name}'
@@ -60,12 +60,15 @@ class ItemModel(DefaultModel):
         verbose_name_plural = 'Модели'
 
 
+# TODO: add logging for StockItem (Related UniqueIdentifier <id> is created. Value: <value>)
+# TODO: add logging for StockItem (Related UniqueIdentifier <id> is changed. New Value: <value>)
+# TODO: add logging for StockItem (Related UniqueIdentifier <id> is deleted.)
 class UniqueIdentifier(DefaultModel):
-    stock_item = models.ForeignKey('stock_items.StockItem', verbose_name='Вещь на складе', on_delete=models.CASCADE)
+    stock_item = models.OneToOneField('stock_items.StockItem', verbose_name='Вещь на складе', on_delete=models.CASCADE)
     value = models.CharField('Значение', max_length=256)
     identifier_type = models.ForeignKey('stock_items.UniqueIdentifierType', verbose_name='Тип уникального идентификатора', on_delete=models.PROTECT)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.value
 
     class Meta:
@@ -82,14 +85,16 @@ class UniqueIdentifierType(DefaultModel):
         verbose_name_plural = 'Типы уникальных идентификаторов'
 
 
+# TODO: add logging for StockItem (Added Warranty #<id>. <Expiration date: <date> if <expiration_date> else Initial Length: <initial_months_length>
+# TODO: add logging for StockItem (Warranty's #<id> length/store_warranty/expiration_date is changed)
 class Warranty(DefaultModel):
-    stock_item = models.ForeignKey('stock_items.StockItem', verbose_name='Вещь на складе', on_delete=models.CASCADE)
+    stock_item = models.OneToOneField('stock_items.StockItem', verbose_name='Вещь на складе', on_delete=models.CASCADE)
     expiration_date = models.DateField('Срок истечения', null=True, blank=True)
-    initial_months_length = models.PositiveIntegerField('Изначальный размер гарантии', null=True, blank=True)
+    initial_months_length = models.PositiveIntegerField('Изначальный размер гарантии', validators=[MinValueValidator(1)], null=True, blank=True)
     store_warranty = models.BooleanField('Гарантия магазина')
     description = models.TextField('Описание')
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'#{self.stock_item.id} | {self.stock_item.name}'
 
     class Meta:
